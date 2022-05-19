5f91a78e7b38e1df862ea35935a4ffff3cefa454
JoeyforJoy <654780480@qq.com>
2022-05-19 22:30:56 +0800

    workflow to publish package

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-/*
- * @Author: JoeyforJoy 654780480@qq.com
- * @Date: 2022-05-19 21:58:44
- * @LastEditors: JoeyforJoy 654780480@qq.com
- * @LastEditTime: 2022-05-19 21:58:50
- * @FilePath: /learning_workflow/index.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 alert("Hello, World!");
